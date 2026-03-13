db818705d81424008af038213c16102fa60cdfcf
kothapallivinaykumar <165812777+vinay2902H@users.noreply.github.com>
2026-03-13 10:38:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 
@@ -81,4 +80,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-
